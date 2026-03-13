80a73f40e73d31e62f8ae98a6080a9a1bd8334ce
JordanGoh4 <lowonmath@gmail.com>
2026-03-13 17:36:41 +0800

    Fix build issue again

--- src/main/java/seedu/address/logic/commands/SortAddressCommand.java
+++ src/main/java/seedu/address/logic/commands/SortAddressCommand.java
@@ -26,6 +26,7 @@ public class SortAddressCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        model.sortPersonList(ADDRESS_COMPARATOR);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
